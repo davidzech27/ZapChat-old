7f64a624378ea186ef02f3e663869ed71bc2dba0
David Z <davidzech27@gmail.com>
2022-04-17 05:02:09 -0700

    messages can be sent

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,27 +1,48 @@
 <script setup>
+    const userData = useUserData()
+
     const profilePopup = ref(false)
 
-    function updateProfile() {
+    const username = ref("")
+    const bio = ref("")
 
+    function updateProfile() {
+        userData.updateProfile({ username: username.value, bio: bio.value })
+
+        profilePopup.value = false
+
+        username.value = ""
+        bio.value = ""
     }
 </script>
 
 
 
 <template>
-<div class="h-screen w-screen backdrop-blur-lg z-10">
+<div>
     <div class="h-2/3 w-1/2 bg-blue absolute left-0 bottom-[-32.4%]" style="border-radius: 50% 100%;"></div>
     <div class="h-screen w-[23.3%] bg-gradient-to-r from-purple to-blue absolute"></div>
     <div class="h-2/3 w-1/4 bg-background absolute rounded-bl-full"></div>
-    <div class="h-[1024px] w-1/2 bg-gradient-to-r from-purple to-blue absolute left-3/4 top-[-56px] animate-wiggle z-10"></div>
+    <div class="h-[1024px] w-1/2 bg-gradient-to-r from-purple to-blue absolute left-3/4 top-[-56px] animate-wiggle"></div>
 
     <button @click="$refs.find.$el.click()" class="bg-blue w-48 h-16 top-[35%] left-[33%] rounded-full absolute tracking-wider text-2xl font-light transition-opacity duration-200 hover:opacity-85">Find room</button>
     <button @click="$refs.create.$el.click()" class="gradient w-56 h-16 top-[35%] right-[33%] rounded-full absolute tracking-wider text-2xl font-light transition-opacity duration-200 hover:opacity-85">Create room</button>
 
-    <button @click="$refs.update.$el.click()" class="bg-purple w-64 h-16 top-[62%] right-[26%] rounded-full absolute tracking-wider text-2xl font-light z-0 transition-opacity duration-200 hover:opacity-85">Update profile</button>
+    <button @click="profilePopup = true" class="bg-purple w-64 h-16 top-[62%] right-[26%] rounded-full absolute tracking-wider text-2xl font-light transition-opacity duration-200 hover:opacity-85">Update profile</button>
 
     <nuxt-link to="/search/find" ref="find"></nuxt-link>
     <nuxt-link to="/search/create" ref="create"></nuxt-link>
-    <nuxt-link to="/a" ref="update"></nuxt-link>
+
+    <div v-show="profilePopup" class="h-screen w-screen bg-background bg-opacity-75 backdrop-filter backdrop-blur-md flex justify-center items-center">
+        <div class="w-1/3 h-2/3 bg-background rounded-2xl border border-white border-opacity-20 grid grid-col pt-32 justify-center">
+            <input type="text" placeholder="username" v-model="username" class="w-84 h-16 text-field bg-surface">
+            <textarea placeholder="bio" v-model="bio" class="w-84 h-24 mt-[-4rem] text-field bg-surface resize-none pt-[1.2rem]"/>
+            <button @click="updateProfile" class="w-84 h-24 mt-[-2rem] rounded-xl bg-gradient-to-tr from-blue to-purple text-4xl tracking-wider transition-opacity duration-200 hover:opacity-85">Update</button>
+        </div>
+    </div>
 </div>
 </template>
+
+<script>
+    import { useUserData } from "~/stores/useUserDataStore"
+</script>
--- pages/room/[roomName].vue
+++ pages/room/[roomName].vue
@@ -1,26 +1,80 @@
 <script setup>
+    const roomName = useRoute().params.roomName
 
+    const userData = useUserData()
+    const { profile } = storeToRefs(userData)
+
+    const room = useRoom()
+    const { room: roomData } = storeToRefs(room)
+
+    const topicText = reactive({
+        topic1: "",
+        topic2: "",
+        topic3: ""
+    })
+
+    onBeforeMount(async () => {
+        room.getRoom(roomName)
+        userData.getProfile()
+    })
+
+    function sendMessage(topicNumber) {
+        if (topicText["topic" + topicNumber] !== "") {
+            room.sendMessage({ topicNumber, text: topicText["topic" + topicNumber] })
+        }
+        
+        topicText["topic" + topicNumber] = ""
+    }
 </script>
 
 <template>
-<div class="grid grid-cols-3">
-    <div class="min-h-screen column overflow-y-scroll p-2.5">
+<div v-if="room.isLoaded" class="grid grid-cols-3">
+    <div class="min-h-screen flex overflow-y-scroll">
+        <div class="w-full h-24 bg-surface tracking-widest font-normal text-4xl flex">
+            <span class="mx-auto self-center">{{ roomData.topic1.name}}</span>
+        </div>
         
         
-        
-        
-        <div class="h-48 w-48 -left-2.5 -bottom-2.5 absolute rounded-tr-full bg-gradient-to-r from-purple to-blue"></div>
+        <div class="w-1/3 h-24 bg-surface bg-opacity-50 absolute bottom-0 z-10 flex">
+            <input @keyup.enter="sendMessage(1)" type="text" :placeholder="'Message ' + roomData.topic1.name" v-model="topicText.topic1" class="w-[26rem] h-16 text-field self-center mx-auto bg-surface focus:bg-background opacity-80">
+        </div>
+
+        <div class="h-48 w-48 left-0 bottom-0 absolute rounded-tr-full bg-gradient-to-r from-purple to-blue"></div>
     </div>
-    <div class="min-h-screen column overflow-y-scroll p-2.5 bg-surface">
-        asdf
+    <div class="min-h-screen flex overflow-y-scroll bg-surface">
+        <div class="w-full h-24 gradient tracking-widest font-normal text-4xl flex">
+            <span class="mx-auto self-center">{{ roomData.topic2.name}}</span>
+        </div>
+
+
+
+        <div class="w-1/3 h-24 absolute bottom-0 z-10 flex">
+            <input @keyup.enter="sendMessage(2)" type="text" :placeholder="'Message ' + roomData.topic2.name" v-model="topicText.topic2" class="w-[26rem] h-16 text-field self-center mx-auto bg-surface focus:bg-background">
+        </div>
     </div>
-    <div class="min-h-screen column overflow-y-scroll p-2.5">
+    <div class="min-h-screen flex overflow-y-scroll">
+        <div class="w-full h-24 tracking-widest font-normal text-4xl flex z-10">
+            <span class="mx-auto self-center">{{ roomData.topic3.name}}</span>
+        </div>
 
+        <div class="w-1/3 h-24 bg-surface bg-opacity-50 absolute bottom-0 z-10 flex">
+            <input @keyup.enter="sendMessage(3)" type="text" :placeholder="'Message ' + roomData.topic3.name" v-model="topicText.topic3" class="w-[26rem] h-16 text-field self-center mx-auto bg-surface bg-opacity-60 focus:bg-opacity-100">
+        </div>
 
-
-        <div class="h-1/3 w-1/4 -right-2.5 -top-2.5 absolute bg-gradient-to-r from-purple to-blue"></div>
+        <div class="h-1/3 w-1/4 right-0 top-0 absolute bg-gradient-to-r from-purple to-blue"></div>
         <div class="h-2/3 w-1/3 -right-2.5 -top-2.5 absolute bg-background" style="border-radius: 50% 100%;"></div>
     </div>
     
 </div>
+<div v-else class="gradient
+            h-screen flex justify-center pt-72
+            text-9xl text-text font-light tracking-wide">
+    <span class="animate-pulse">Loading...</span>
+</div>
 </template>
+
+<script>
+    import { useUserData } from "~/stores/useUserDataStore"
+    import { useRoom } from "~/stores/useRoomStore"
+    import { storeToRefs } from "pinia"
+</script>
