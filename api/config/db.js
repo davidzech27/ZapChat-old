33ff2f13261fb9fb0ad831bfb64dd37bb71945a0
David Z <davidzech27@gmail.com>
2022-04-27 22:29:14 -0700

    db settings

--- api/config/db.js
+++ api/config/db.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose"
 
 export default () => {
-    try {
-        mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true, autoIndex: true })//! set autoIndex to false when indexes have been finalized
-        mongoose.set("returnOriginal", false)
-        console.log("connected to database")
-    }
-    catch (err) {
-        console.error(err)
-    }
+	try {
+		mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true, autoIndex: false }) //! set autoIndex to false when indexes have been finalized
+		mongoose.set("returnOriginal", false)
+		console.log("connected to database")
+	} catch (err) {
+		console.error(err)
+	}
 }
